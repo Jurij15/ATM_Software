f0831a02752287b59f88824c7b2c814841a0b3e4
Jurij15 <jurij.grom1@gmail.com>
2022-07-18 14:28:06 +0200

    Add project files.

---
+++ ATM_Software/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATM_Software // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //this will be used as a logger, window will appear on start
+        }
+    }
+}
