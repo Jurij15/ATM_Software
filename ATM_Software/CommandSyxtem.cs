16f3a2ebdb554ef947dba8136d2808c9e4f620a0
Jurij15 <jurij.grom1@gmail.com>
2022-07-23 01:38:01 +0200

    added a command

---
+++ ATM_Software/CommandSyxtem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Software
+{
+    public static class CommandSyxtem
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Enter a command:");
+            string input = Console.ReadLine();
+
+            while (input == input)
+            {
+                if (input.Contains(Commands.ChangeValidCommand))
+                {
+                    Console.WriteLine("tbd");
+                }
+            }
+        }
+    }
+}
---
+++ ATM_Software/Commands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_Software
+{
+    public static class Commands
+    {
+        public static string ChangeValidCommand = "ChangeValid";
+    }
+}
